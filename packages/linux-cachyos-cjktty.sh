f3b623d414fd64307e3f083d069fabda6b415344
ShinKouyo <i@0x0f.dev>
2026-04-05 15:33:18 +0800

    chore: update

---
+++ packages/linux-cachyos-cjktty.sh
@@ -0,0 +1,17 @@
+# shellcheck disable=all
+
+SCHEMA_VERSION=1
+NAME='linux-cachyos-cjktty'
+
+SOURCE_GIT='https://aur.archlinux.org/linux-cachyos-cjktty.git'
+SOURCE_REF='master'
+
+BUILD_WORKDIR='.'
+BUILD_PKGBUILD='./PKGBUILD'
+
+UPDATE_ENABLED=1
+UPDATE_VCS=0
+
+build_env() {
+    :
+}
