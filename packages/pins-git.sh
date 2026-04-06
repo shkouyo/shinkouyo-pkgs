7cfed30381d9ae4dd5c9b0dd91e42e72649b21a0
ShinKouyo <i@0x0f.dev>
2026-04-06 10:47:50 +0800

    chore: add packages (pins-git)

---
+++ packages/pins-git.sh
@@ -0,0 +1,17 @@
+# shellcheck disable=all
+
+SCHEMA_VERSION=1
+NAME='pins-git'
+
+SOURCE_GIT='https://aur.archlinux.org/pins-git.git'
+SOURCE_REF='master'
+
+BUILD_WORKDIR='.'
+BUILD_PKGBUILD='./PKGBUILD'
+
+UPDATE_ENABLED=1
+UPDATE_VCS=1
+
+build_env() {
+    :
+}
