2e6923323a48340550c964f41044ecb938030df0
ShinKouyo <i@0x0f.dev>
2026-04-06 09:54:13 +0800

    chore: add packages (noctalia-qs, noctalia-shell)

---
+++ packages/noctalia-shell.sh
@@ -0,0 +1,17 @@
+# shellcheck disable=all
+
+SCHEMA_VERSION=1
+NAME='noctalia-shell'
+
+SOURCE_GIT='https://aur.archlinux.org/noctalia-shell.git'
+SOURCE_REF='master'
+
+BUILD_WORKDIR='.'
+BUILD_PKGBUILD='./PKGBUILD'
+
+UPDATE_ENABLED=1
+UPDATE_VCS=0
+
+build_env() {
+    :
+}
