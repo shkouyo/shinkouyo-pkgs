2f59789b3c672411f927acda4690ec4c236d493d
ShinKouyo <i@0x0f.dev>
2026-04-06 10:27:16 +0800

    chore: add packages (linuxqq)

---
+++ packages/linuxqq.sh
@@ -0,0 +1,17 @@
+# shellcheck disable=all
+
+SCHEMA_VERSION=1
+NAME='linuxqq'
+
+SOURCE_GIT='https://aur.archlinux.org/linuxqq.git'
+SOURCE_REF='master'
+
+BUILD_WORKDIR='.'
+BUILD_PKGBUILD='./PKGBUILD'
+
+UPDATE_ENABLED=1
+UPDATE_VCS=0
+
+build_env() {
+    :
+}
