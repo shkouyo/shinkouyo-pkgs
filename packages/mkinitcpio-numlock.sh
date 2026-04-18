1cf07019a27fdc7d710531a73a9a68dc0f688854
深渊架构ShinKouyo <i@0x0f.dev>
2026-04-18 13:17:39 +0800

    chore: update (mkinitcpio-numlock)

---
+++ packages/mkinitcpio-numlock.sh
@@ -0,0 +1,17 @@
+# shellcheck disable=all
+
+SCHEMA_VERSION=1
+NAME='pkgbase'
+
+SOURCE_GIT='https://aur.archlinux.org/mkinitcpio-numlock.git'
+SOURCE_REF='master'
+
+BUILD_WORKDIR='.'
+BUILD_PKGBUILD='./PKGBUILD'
+
+UPDATE_ENABLED=1
+UPDATE_VCS=0
+
+build_env() {
+    :
+}
