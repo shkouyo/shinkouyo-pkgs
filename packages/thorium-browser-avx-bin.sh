1e0a76fbd1da2ff7762f79be90df8116c4a06a34
ShinKouyo <i@0x0f.dev>
2026-05-16 22:55:52 +0800

    thorium-browser-bin, thorium-browser-avx-bin, thorium-browser-avx2-bin

---
+++ packages/thorium-browser-avx-bin.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=sh
+
+SCHEMA_VERSION=1
+NAME='thorium-browser-avx-bin'
+
+SOURCE_GIT='https://aur.archlinux.org/thorium-browser-avx-bin.git'
+SOURCE_REF='master'
+
+BUILD_WORKDIR='.'
+BUILD_PKGBUILD='./PKGBUILD'
+
+UPDATE_ENABLED=1
+UPDATE_VCS=0
+
+build_env() {
+    :
+}
