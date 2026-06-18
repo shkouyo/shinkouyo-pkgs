b5b84fd532f8ed23d7a171c14d52de3158742d99
深渊架构ShinKouyo <i@0x0f.dev>
2026-06-18 22:57:11 +0800

    chore: add linux-sk

---
+++ packages/linux-sk.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=sh
+
+SCHEMA_VERSION=1
+NAME='linux-sk'
+
+SOURCE_GIT='https://aur.archlinux.org/linux-sk.git'
+SOURCE_REF='master'
+
+BUILD_WORKDIR='.'
+BUILD_PKGBUILD='./PKGBUILD'
+
+UPDATE_ENABLED=1
+UPDATE_VCS=0
+
+build_env() {
+    :
+}
